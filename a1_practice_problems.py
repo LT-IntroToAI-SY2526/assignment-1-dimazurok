8484304e34dfbad80319ece1175b47ab20b5616b
Daniel Mazurok <dimazurok@cps.edu>
2025-09-04 10:53:42 -0500

    finished absolute function

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -27,8 +27,10 @@ def absolute(n: int) -> int:
     Returns:
         the absolute value of the passed in number
     """
-    raise NotImplementedError("absolute")
-
+    if n < 0:
+        return -1 * n
+    else:
+        return n
 
 def factorial(n: int) -> int:
     """Takes a number n, and computes the factorial n! You can assume the passed in
