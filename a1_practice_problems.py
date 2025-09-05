cc424db501cbd0291033749c7a7aed50ad718bc5
Daniel Mazurok <dimazurok@cps.edu>
2025-09-05 11:19:51 -0500

    added an assert

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -127,6 +127,7 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
 # this line causes the nested code to be skipped if the file is imported instead of run
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
+    assert absolute(5) == 5, "abosulte of 5 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
     assert every_other([1, 2, 3, 4, 5]) == [
         1,
